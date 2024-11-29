5e91728fe12a5b990a16c0945b43d79b7e30874b
Abijith CB <122815590+cbabi2023@users.noreply.github.com>
2024-11-29 13:38:59 +0530

    Add files via upload

---
+++ chats/include/init.php
@@ -0,0 +1,121 @@
+<script data-cfasync="false">var SB_AJAX_URL = "https://rentals.imastudent.com/chats/include/ajax.php";var SB_URL = "https://rentals.imastudent.com/chats";var SB_LANG = ["en","front"];</script><style>.sb-chat-btn, .sb-chat>div>.sb-header,.sb-chat .sb-dashboard>div>.sb-btn:hover,.sb-chat .sb-scroll-area .sb-header,.sb-input.sb-input-btn>div,div ul.sb-menu li:hover,
+                 .sb-select ul li:hover,.sb-popup.sb-emoji .sb-emoji-bar>div.sb-active, .sb-popup.sb-emoji .sb-emoji-bar>div:hover,.sb-btn,a.sb-btn,.sb-rich-message[disabled] .sb-buttons .sb-btn,
+                 .sb-ul>span:before,.sb-article-category-links>span+span:before { background-color: rgb(199, 8, 81); }.sb-chat .sb-dashboard>div>.sb-btn,.sb-search-btn>input,.sb-input>input:focus, .sb-input>select:focus, .sb-input>textarea:focus,
+                 .sb-input.sb-input-image .image:hover { border-color: rgb(199, 8, 81); }.sb-chat .sb-dashboard>div>.sb-btn,.sb-editor .sb-bar-icons>div:hover:before,.sb-articles>div:hover>div,.sb-main .sb-btn-text:hover,.sb-editor .sb-submit,.sb-table input[type="checkbox"]:checked:before,
+                 .sb-select p:hover,div ul.sb-menu li.sb-active, .sb-select ul li.sb-active,.sb-search-btn>i:hover,.sb-search-btn.sb-active i,.sb-rich-message .sb-input>span.sb-active:not(.sb-filled),
+                 .sb-input.sb-input-image .image:hover:before,.sb-rich-message .sb-card .sb-card-btn,.sb-slider-arrow:hover,.sb-loading:not(.sb-btn):before,.sb-articles>div.sb-title,.sb-article-categories>div:hover, .sb-article-categories>div.sb-active,
+                 .sb-article-categories>div span:hover,.sb-article-categories>div span.sb-active,.sb-btn-text:hover,.sb-player > div:hover,.sb-loader:before { color: rgb(199, 8, 81); }.sb-search-btn>input:focus,.sb-input>input:focus, .sb-input>select:focus, .sb-input>textarea:focus,.sb-input.sb-input-image .image:hover { box-shadow: 0 0 5px rgba(104, 104, 104, 0.2); }.sb-list>div.sb-rich-cnt { border-top-color: rgb(199, 8, 81); }.sb-list>div.sb-right .sb-message, .sb-list>div.sb-right .sb-message a { color: #566069; } .sb-list>div.sb-right,.sb-right .sb-player>div { background-color: #f0f0f0; }.sb-chat-btn:hover,.sb-input.sb-input-btn>div:hover,.sb-btn:hover,a.sb-btn:hover,.sb-rich-message .sb-card .sb-card-btn:hover { background-color: rgb(199, 8, 81); }.sb-list>.sb-right .sb-message, .sb-list>.sb-right .sb-message a,.sb-editor .sb-submit:hover { color: rgb(199, 8, 81); }.sb-list>.sb-right,.sb-user-conversations>li:hover { background-color: rgba(26, 24, 24, 0.01); }.sb-chat-btn { bottom: 75px; }</style><style>@font-face {
+                    font-family: "Support Board Font";
+                    src: url("https://www.imastudent.com/wwwroot/chat-files/fonts/regular.woff2") format("woff2");
+                    font-weight: 400;
+                    font-style: normal;
+                }
+                @font-face {
+                    font-family: "Support Board Font";
+                    src: url("https://www.imastudent.com/wwwroot/chat-files/fonts/medium.woff2") format("woff2");
+                    font-weight: 500;
+                    font-style: normal;
+                }
+                @font-face {
+                    font-family: "Support Board Icons";
+                    src: url("https://www.imastudent.com/wwwroot/chat-files/icons/support-board.eot?v=2");
+                    src: url("https://www.imastudent.com/wwwroot/chat-files/icons/support-board.eot?#iefix") format("embedded-opentype"), url("https://www.imastudent.com/wwwroot/chat-files/icons/support-board.woff?v=2") format("woff"), url("https://www.imastudent.com/wwwroot/chat-files/icons/support-board.ttf?v=2") format("truetype"), url("https://www.imastudent.com/wwwroot/chat-files/icons/support-board.svg#support-board?v=2") format("svg");
+                    font-weight: normal;
+                    font-style: normal;
+                }
+                </style>    <div class="sb-main sb-chat sb-no-conversations" style="display: none; transition: none;">
+        <div class="sb-body">
+            <div class="sb-scroll-area sb-texture-2">
+                <div class="sb-header sb-header-main sb-header-type-agents" >
+                    <i class="sb-icon-close sb-dashboard-btn"></i>
+                    <div class="sb-content">
+                                                <div class="sb-title">
+                            Chat with us! 👋                        </div>
+                        <div class="sb-text">
+                            Welcome to IMASTUDENT. Ask us anything 👊                        </div>
+                        <div class="sb-profiles"><div><span>Deepak</span><script type="text/javascript" style="display:none">
+//<![CDATA[
+window.__mirage2 = {petok:"8HvKqji83tUUfzwPzCKavTqkodIBeS0BeG5Z_gQgSb8-1800-0.0.1.1"};
+//]]>
+</script>
+<script type="text/javascript" src="https://ajax.cloudflare.com/cdn-cgi/scripts/04b3eb47/cloudflare-static/mirage2.min.js"></script>
+<img loading="lazy" alt="" data-cfsrc="https://rentals.imastudent.com/chats/uploads/23-06-21/shortlogo-imastudent.jpg" style="display:none;visibility:hidden;" /><noscript><img src="https://rentals.imastudent.com/chats/uploads/23-06-21/shortlogo-imastudent.jpg" loading="lazy" alt=""/></noscript></div><div><span>Karthik</span><img loading="lazy" alt="" data-cfsrc="https://rentals.imastudent.com/chats/uploads/23-06-21/6930408.png" style="display:none;visibility:hidden;" /><noscript><img src="https://rentals.imastudent.com/chats/uploads/23-06-21/6930408.png" loading="lazy" alt=""/></noscript></div><div><span>Gunjan</span><img loading="lazy" alt="" data-cfsrc="https://rentals.imastudent.com/chats/uploads/20-11-23/7432603.png" style="display:none;visibility:hidden;" /><noscript><img src="https://rentals.imastudent.com/chats/uploads/20-11-23/7432603.png" loading="lazy" alt=""/></noscript></div></div>                    </div>
+                    <div class="sb-label-date-top"></div>
+                </div>
+                <div class="sb-list sb-active"></div>
+                <div class="sb-dashboard">
+                    <div class="sb-dashboard-conversations">
+                        <div class="sb-title">
+                            Conversations                        </div>
+                        <ul class="sb-user-conversations"></ul>
+                        <div class="sb-btn sb-btn-new-conversation">New conversation</div><div class="sb-btn sb-btn-all-conversations">View all</div>                    </div>
+                    <div class="sb-dashboard-articles"><div class="sb-title">Help Center</div><div class="sb-input sb-input-btn"><input placeholder="Search for articles..." autocomplete="off"><div class="sb-submit-articles sb-icon-search"></div></div><div class="sb-articles"></div><div class="sb-btn sb-btn-all-articles">All articles</div></div><div data-success="" class="sb-info"></div>                </div>
+                <div class="sb-panel sb-panel-articles"></div>
+            </div>
+                <div class="sb-editor">
+                <div class="sb-textarea">
+            <textarea placeholder="Write a message..."></textarea>
+        </div>
+        <div class="sb-attachments"></div>
+        <div class="sb-bar"><div class="sb-bar-icons"><div class="sb-btn-attachment" data-sb-tooltip="Attach a file"></div><div class="sb-btn-saved-replies" data-sb-tooltip="Add a saved reply"></div><div class="sb-btn-audio-clip" data-sb-tooltip="Voice message"></div><div class="sb-btn-emoji" data-sb-tooltip="Add an emoji"></div></div><div class="sb-icon-send sb-submit" data-sb-tooltip="Send message"></div><i class="sb-loader"></i></div>        <div class="sb-popup sb-emoji">
+            <div class="sb-header">
+                <div class="sb-select">
+                    <p>
+                        All                    </p>
+                    <ul>
+                        <li data-value="all" class="sb-active">
+                            All                        </li>
+                        <li data-value="Smileys">
+                            Smileys & Emotions                        </li>
+                        <li data-value="People">
+                            People & Body                        </li>
+                        <li data-value="Animals">
+                            Animals & Nature                        </li>
+                        <li data-value="Food">
+                            Food & Drink                        </li>
+                        <li data-value="Travel">
+                            Travel & Places                        </li>
+                        <li data-value="Activities">
+                            Activities                        </li>
+                        <li data-value="Objects">
+                            Objects                        </li>
+                        <li data-value="Symbols">
+                            Symbols                        </li>
+                    </ul>
+                </div>
+                <div class="sb-search-btn">
+                    <i class="sb-icon sb-icon-search"></i>
+                    <input type="text" placeholder="Search emoji..." />
+                </div>
+            </div>
+            <div class="sb-emoji-list">
+                <ul></ul>
+            </div>
+            <div class="sb-emoji-bar"></div>
+        </div>
+                <form class="sb-upload-form-editor" action="#" method="post" enctype="multipart/form-data">
+            <input type="file" name="files[]" class="sb-upload-files" multiple />
+        </form>
+        <div id="sb-audio-clip">
+            <div class="sb-icon sb-icon-close"></div>
+            <div class="sb-audio-clip-cnt">
+                <div class="sb-audio-clip-time"></div>
+                <div class="sb-icon sb-icon-play sb-btn-clip-player"></div>
+            </div>
+            <div class="sb-icon sb-icon-pause sb-btn-mic"></div>
+        </div>
+    </div>
+    <div class="sb-overlay-panel"><div><div></div><i class="sb-btn-icon sb-icon-close"></i></div><div></div></div>        </div>
+        <div class="sb-chat-btn">
+            <span data-count="0"></span>
+            <img class="sb-icon" alt="" src="https://rentals.imastudent.com/chats/media/button-chat-2.svg" />
+            <img class="sb-close" alt="" src="https://rentals.imastudent.com/chats/media/button-close.svg" />
+        </div>
+        <i class="sb-icon sb-icon-close sb-responsive-close-btn"></i>
+                <div class="sb-lightbox-media">
+            <div></div>
+            <i class="sb-icon-close"></i>
+        </div>
+        <div class="sb-lightbox-overlay"></div>
+    </div>
+<!-- Support Board - https://board.support -->
